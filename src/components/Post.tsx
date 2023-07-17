6e1df41fab340cc2f5a11d39925217650629f4df
ayakuma309 <ayako.30901@gmail.com>
2023-07-18 00:39:36 +0900

    feat: プロフィールページに行くリンク

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,5 @@
 import { PostType } from '@/types/types'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {
@@ -11,12 +12,14 @@ const Post:React.FC<Props> = ({post}) => {
       <div className="bg-white shadow-md rounded p-4 mb-4">
         <div className="mb-4">
           <div className="flex items-center mb-2">
-            <img
-              className="w-10 h-10 rounded-full mr-2"
-              src={post.author.profile?.profileImageUrl}
-              alt="User Avatar"
-            />
-            
+            <Link href={`/profile/${post.authorId}`}>
+              <img
+                src={post.author.profile?.profileImageUrl}
+                className="w-10 h-10 rounded-full mr-2"
+                alt="User Avatar"
+              />
+            </Link>
+
             <div>
               <h2 className="font-semibold text-md">{post.author?.username}</h2>
               <p className="text-gray-500 text-sm">
